23e43e47240a0e3c71da3e993721d663fa7a5355
jmforsythe <72832948+jmforsythe@users.noreply.github.com>
2026-02-17 01:43:54 +0000

    Add season 2012-2013

--- fetch_addresses.py
+++ fetch_addresses.py
@@ -68,6 +68,9 @@ def team_name_to_club_name(team_name: str) -> str:
     last_word = team_name.split(" ")[-1]
     if last_word in ["II", "III", "IV"]:
         return " ".join(team_name.split(" ")[:-1])
+    last_two_words = " ".join(team_name.split(" ")[-2:])
+    if last_two_words in ["2nd XV", "3rd XV", "4th XV"]:
+        return " ".join(team_name.split(" ")[:-2])
     return team_name
 
 
